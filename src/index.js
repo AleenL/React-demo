2117123b5309377aaf3cffc290460a162782ac9c
784757555@qq.com <784757555@qq.com>
2017-04-24 09:49:57 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,20 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
-function tick() {
-	const element = (
-		<div>
-			<h1>Hello, world</h1>
-			<h2>It is {new Date().toLocaleTimeString()}.</h2>
-		</div>
-	);
+import Welcome from './Welcome';
 
 
-	ReactDOM.render(
-		element,
-		document.getElementById('root')
-	);
-}
-
-setInterval(tick,1000);
+ReactDOM.render(
+  <Welcome/>,
+  document.getElementById('root')
+)
