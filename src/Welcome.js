dc21147f1e8b1fffe17d1483fecb8bced40c7f27
784757555@qq.com <784757555@qq.com>
2017-04-24 12:15:06 +0800

    update

--- src/Welcome.js
+++ src/Welcome.js
@@ -11,15 +11,22 @@ class Welcome extends React.Component {
 		date: new Date() 
 	})
     })
+    console.log('我已经在constructor里将props和state初始化好了')
    }
-
+   componentWillMount(){
+     console.log('运行到这里的话，说明吗马上就要运行render了')
+   }
   render() {
+      console.log('这里是render')
   	return (
   		<div>
   			<h1>Hello,{this.props.name}</h1>
   			<h2>{this.state.date.toString()}</h2>
   		</div>
   		)
+  }
+  compontentDidMount(){
+    console.log('已经挂载页面了')
   }
 }
 
