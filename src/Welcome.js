0edf764681016489b7bacf2f2c06f7d82dde3997
784757555@qq.com <784757555@qq.com>
2017-04-24 10:21:07 +0800

    update

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,12 +1,17 @@
 import React from 'react'; 
 class Welcome extends React.Component {
-  constructor(props){
-  	super(props)
-
-  	this.state = {
-  		date: new Date()
-  	}
-  }
+      constructor(props){
+      super(props)
+     
+     this.state = {
+       date: new Date()
+     }
+  setInterval(()=>{
+    this.setState({
+		date: new Date() 
+	})
+    })
+   }
 
   render() {
   	return (
