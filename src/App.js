88257146da53322f84844934c2d78126445baa7c
784757555@qq.com <784757555@qq.com>
2017-04-24 13:23:24 +0800

    将每个代办封装成TodoItem

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import todoInput from './todoInput'
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
 
 class App extends Component {
   constructor(props){
@@ -8,20 +9,25 @@ class App extends Component {
     this.state = {
       newTodo: 'test',
       todoList:[
-        {id:1, title:'第一个代办'}
+        {id:1, title:'第一个代办'},
+        {id:2, title:'第二个代办'}
       ]
     }
   }
   render() {
     let todos = this.state.todoList.map((item,index)=>{
-      return <li>{item.title}</li>
+      return (
+        <li>
+          <TodoItem todo={item} />
+        </li>
+      )
     })
 
     return(
       <div className='App'>
         <h1>我的代办</h1>
         <div className='inputWrapper'>
-          <todoInput content={this.state.newTodo} />
+          <TodoInput content={this.state.newTodo} />
         </div>
         <ol>
           {todos}
