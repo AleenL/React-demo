bff1c1c714dc6963be40637960e32a21cb6484d6
784757555@qq.com <784757555@qq.com>
2017-04-24 09:38:41 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <div><h1>Hello, world!</h1><h2>Allen Lee</h2></div>,
-  document.getElementById('root')
-);
+function tick() {
+	const element = (
+		<div>
+			<h1>Hello, world</h1>
+			<h2>It is {new Date().toLocaleTimeString()}.</h2>
+		</div>
+	);
+
+
+	ReactDOM.render(
+		element,
+		document.getElementById('root')
+	);
+}
+
+setInterval(tick,1000);
