b5110fe36aeec51cf1811a11aebe6ee7e5aa3d63
784757555@qq.com <784757555@qq.com>
2017-04-24 14:03:44 +0800

    listen Enter

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default class TodoInput extends React.Component {
 	render() {
-		return <input typr="text" defaultValue={this.props.content} />
+		return <input typr="text" defaultValue={this.props.content} onKeyPress={this.submit}/>
+
+	}
+
+	submit(e){
+		if (e.key === "Enter"){
+			console.log('press Enter')
+		}
 	}
 }
