3b332bed2e4857c7011d1816caa1e19b6b2d7730
784757555@qq.com <784757555@qq.com>
2017-04-24 12:31:08 +0800

    update

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,33 +1,69 @@
 import React from 'react'; 
 class Welcome extends React.Component {
-      constructor(props){
-      super(props)
+    constructor(props){
+    super(props)
      
-     this.state = {
-       date: new Date()
+    this.state = {
+    	date: new Date(),
+    	test: '1'
      }
-  setInterval(()=>{
+
     this.setState({
-		date: new Date() 
-	})
+    	date: new Date(),
+    	test: 'constructor'
     })
     console.log('我已经在constructor里将props和state初始化好了')
-   }
-   componentWillMount(){
-     console.log('运行到这里的话，说明吗马上就要运行render了')
-   }
-  render() {
-      console.log('这里是render')
+   	}
+
+   	componentWillMount(){
+   		this.setState({
+   			date: new Date(),
+   			test: 'componentWillMount'
+   		})
+   		console.log('运行到这里的话，说明吗马上就要运行render了')
+   	}
+  	render() {
+  		this.setState({
+  			date: new Date(),
+  			test: 'render'
+  		})
+    	console.log('这里是render')
   	return (
   		<div>
   			<h1>Hello,{this.props.name}</h1>
   			<h2>{this.state.date.toString()}</h2>
+  			<p>{test}</p>
   		</div>
   		)
-  }
-  compontentDidMount(){
-    console.log('已经挂载页面了')
-  }
+  	}
+  	componentDidMount(){
+  		this.setState({
+  			date: new Date(),
+  			test: 'componentDidMount'
+  		})
+    	console.log('已经挂载页面了')
+  	}
+  	componentWillReceiveProps(){
+  		this.setState({
+  			date: new Date(),
+  			test: 'componentWillReceiveProps'
+  		})
+  	}
+  	shouldComponentUpdate(){
+  		this.setState({
+  			date: new Date(),
+  			test: 'shouldComponentUpdate'
+  		})
+  	}
+  	componentWillUpdate(){
+  		this.setState({
+  			date: new Date(),
+  			test: 'componentWillUpdate'
+  		})
+  	}
+  	componentWillUnmount(){
+  		console.log('dead')
+  	}
 }
 
 export default Welcome
