fffcc59b986ad47bfd4c8040adb4125e377fe480
784757555@qq.com <784757555@qq.com>
2017-04-25 09:47:53 +0800

    build new leanCloud

---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'uPBpX68bTuHXQalB6IiIirYh-gzGzoHsz';
+var APP_KEY = 'qeSqnYMF8YbKpfiUxhH5hOBV';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV
