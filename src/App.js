58116918c951277a0a8120bbf10f5082c322857d
784757555@qq.com <784757555@qq.com>
2017-04-25 14:50:13 +0800

    add signOut

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './reset.css'
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import UserDialog from './UserDialog'
-import {getCurrentUser} from './leanCloud'
+import {getCurrentUser, signOut} from './leanCloud'
 
 
 
@@ -32,7 +32,8 @@ class App extends Component {
     })
     return(
       <div className='App'>
-        <h1>{this.state.user.username||'我'}的待办</h1>
+        <h1>{this.state.user.username||'我'}的待办
+        {this.state.user.id ? <button onClick={this.signOut.bind(this)}>登出</button> : null}</h1>
         <div className='inputWrapper'>
           <TodoInput content={this.state.newTodo} 
           onChange={this.changeTitle.bind(this)} 
@@ -46,9 +47,14 @@ class App extends Component {
     )
   }
     onSignUp(user){
-    let stateCopy = JSON.parse(JSON.stringify(this.state)) 
-    stateCopy.user = user
-    this.setState(stateCopy)
+      let stateCopy = JSON.parse(JSON.stringify(this.state)) 
+      stateCopy.user = user
+      this.setState(stateCopy)
+    }
+    signOut(){
+      let stateCopy = JSON.parse(JSON.stringify(this.state))
+      stateCopy.user = {}
+      this.setState(stateCopy)
     }
     componentDidUpdate(){
       
