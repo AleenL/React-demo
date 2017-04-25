126d6c96f96c55f535e9013ddb453d63cdf74990
784757555@qq.com <784757555@qq.com>
2017-04-25 08:47:46 +0800

    save & load

--- src/App.js
+++ src/App.js
@@ -39,22 +39,22 @@ class App extends Component {
       </div>
     )
   }
+    componentDidUpdate(){
+      localStore.save('todoList', this.state.todoList)
+    }
     delete(enevt, todo){
       todo.deleted = true
       this.setState(this.state)
-      localStore.save('todoList', this.state.todoList)
     }
     toggle(e, todo){
       todo.status = todo.status === 'completed' ? '' : 'completed'
       this.setState(this.state)
-      localStore.save('todoList', this.state.todoList)
     }
     changeTitle(event){
       this.setState({
         newTodo: event.target.value,
         todoList: this.state.todoList
       })
-      localStore.save('todoList', this.state.todoList)
     }
     addTodo(event){
       this.state.todoList.push({
@@ -67,7 +67,6 @@ class App extends Component {
         newTodo: '',
         todoList: this.state.todoList
       })
-      localStore.save('todoList', this.state.todoList)
     }
   }
 
